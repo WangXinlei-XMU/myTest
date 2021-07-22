2fcc96d8298e462e8f728d6e367b18dd2a01c599
Administrator <2136805207@qq.com>
2021-07-22 20:21:50 +0800

    vue导航，首页初步设计

--- front/src/main.js
+++ front/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router"
+import router from "./router/index.js"
+import Element from 'element-ui'
 
-
+Vue.use(Element);
 Vue.use(VueRouter);
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ front/src/views/Main.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="ii">
+    <form>
+      <label for="username">用户名</label><input type="text" v-model.trim="loginData.username" name="username">
+      <label for="password">用户名</label><input type="password" v-model.trim="loginData.password" name="password">
+      <input type="button" value="登录" @click="doLogin">
+    </form>
+  </div>
+</template>
